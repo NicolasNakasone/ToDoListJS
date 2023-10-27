50999756fb5bc1e73d85742946c87984c4ef85f9
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2023-10-27 13:37:57 -0300

    feat(delete): added event listener to delete a to do item
    * Also it have been added some styles

--- script.js
+++ script.js
@@ -2,26 +2,42 @@ const toDoList = document.getElementById("to_do_list");
 const toDoForm = document.getElementById("to_do_form");
 const toDoInput = document.getElementById("to_do_input");
 
-function handleKeyPress(event) {
+const handleKeyPress = (event) => {
   if (event.key === "Enter") {
     event.preventDefault();
   }
-}
+};
 
-function handleSubmit(event) {
+const handleSubmit = (event) => {
   event.preventDefault();
   const { value: inputValue } = toDoInput;
   if (inputValue) {
     addItemToList(inputValue);
     toDoInput.value = "";
   }
-}
+};
 
-function addItemToList(itemText) {
+const removeItemFromList = (item) => {
+  toDoList.removeChild(item);
+};
+
+const addItemToList = (itemText) => {
   const newItem = document.createElement("li");
-  newItem.textContent = itemText;
+
+  const itemSpan = document.createElement("span");
+  itemSpan.textContent = itemText;
+  newItem.appendChild(itemSpan);
+
+  newItem.setAttribute("class", "to_do_item");
+
+  const itemButton = document.createElement("button");
+  itemButton.textContent = "X";
+
+  newItem.appendChild(itemButton);
   toDoList.appendChild(newItem);
-}
+
+  itemButton.addEventListener("click", () => removeItemFromList(newItem));
+};
 
 toDoInput.addEventListener("keypress", handleKeyPress);
 
