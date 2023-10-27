4c9ba36b00fac0a86d03d7801ca6acb5df9597f8
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2023-10-27 15:43:01 -0300

    refactor(code): moved inline function to a new constant
    * Also it have been modified some styles

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const handleKeyPress = (event) => {
 
 const handleSubmit = (event) => {
   event.preventDefault();
+
   const { value: inputValue } = toDoInput;
   if (inputValue) {
     addItemToList(inputValue);
@@ -17,17 +18,24 @@ const handleSubmit = (event) => {
   }
 };
 
-const removeItemFromList = (item) => {
-  toDoList.removeChild(item);
+const removeItemFromList = (itemToRemove) => {
+  toDoList.removeChild(itemToRemove);
+};
+
+const toggleTask = (newItem) => {
+  const textDecorationValue = newItem.firstChild.style.textDecoration
+    ? ""
+    : "line-through";
+  newItem.firstChild.style.textDecoration = textDecorationValue;
 };
 
 const addItemToList = (itemText) => {
   const newItem = document.createElement("li");
-
   const itemSpan = document.createElement("span");
-  itemSpan.textContent = itemText;
-  newItem.appendChild(itemSpan);
 
+  itemSpan.textContent = itemText;
+
+  newItem.appendChild(itemSpan);
   newItem.setAttribute("class", "to_do_item");
 
   const itemButton = document.createElement("button");
@@ -36,9 +44,9 @@ const addItemToList = (itemText) => {
   newItem.appendChild(itemButton);
   toDoList.appendChild(newItem);
 
+  newItem.addEventListener("click", () => toggleTask(newItem));
   itemButton.addEventListener("click", () => removeItemFromList(newItem));
 };
 
 toDoInput.addEventListener("keypress", handleKeyPress);
-
 toDoForm.addEventListener("submit", handleSubmit);
